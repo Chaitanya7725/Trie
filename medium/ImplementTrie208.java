6acaee454f8d1abad6200e511b278646f3348305
Chaitanya7725 <chaitanya7725@gmail.com>
2024-04-10 19:59:17 -0500

    Please refer the leetcode solution for the Trie Data structure

---
+++ medium/ImplementTrie208.java
@@ -0,0 +1,22 @@
+package medium;
+
+// Refer this link: https://leetcode.com/problems/implement-trie-prefix-tree/
+class Node {
+
+    boolean isWord;
+    char val;
+    Node [] children;
+
+    public Node(boolean isWord, char val, Node[] children) {
+        this.isWord = isWord;
+        this.val = val;
+        this.children = children;
+    }
+}
+public class ImplementTrie208 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
